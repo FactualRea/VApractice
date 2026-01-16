b6cb990ab3e13328e71d258be380eb5e8148db35
Reatlegile <rmatsepe30@gmail.com>
2026-01-16 14:15:37 +0200

    16 Jan

---
+++ Loops/A_loops_exercise/count_up.py
@@ -0,0 +1,6 @@
+def count_up(max_num):
+    for n in range(1, max_num + 1):
+        print(n)
+        
+count_up(5)
+count_up(3)
---
+++ Loops/A_loops_exercise/one_to_four.py
@@ -0,0 +1,5 @@
+def one_to_four():
+    for number in range(1, 5):
+        print(number)
+        
+one_to_four()
