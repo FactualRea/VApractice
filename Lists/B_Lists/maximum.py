46932baca7ffca5f6cb688f53904fe31e2f35282
Reatlegile <rmatsepe30@gmail.com>
2026-01-26 15:55:16 +0200

    word_count

--- Lists/B_Lists/maximum.py
+++ Lists/B_Lists/maximum.py
@@ -3,10 +3,9 @@ def maximum(numbers):
     for num in numbers:
         if len(numbers) == 0:
             return None
-        if max_num < num:
+        elif max_num < num:
             max_num = num
     print(max_num)
-
 
 
 maximum([5, 6, 3, 7])
--- Lists/B_Lists/word_count.py
+++ Lists/B_Lists/word_count.py
@@ -1,9 +1,9 @@
 def word_count(sentence, target_words):
     count = 0
-    for word in sentence:
+    for word in sentence.split(" "):
         if word in target_words:
             count += 1
-        print(count)
+    print(count)
     
 word_count("open the window please", ["please", "open", "sorry"])
 word_count("drive to the cinema", ["the", "driver"])
