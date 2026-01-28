092b981c2e042a376b1c44e8da6e9798cbc58d70
Reatlegile <rmatsepe30@gmail.com>
2026-01-28 13:21:19 +0200

    snp4

---
+++ Nesting/A_Nesting/snippet_4.py
@@ -0,0 +1,5 @@
+locations = ["flatbush", "williamsburg", "bushwick", "greenpoint"]
+
+for i in range(len(locations)):
+    for j in range(i + 1, len(locations)):
+        print(locations[i], locations[j])
