d3fcf81828cac717b11fbe423d53fa3b5494a4a1
Reatlegile <rmatsepe30@gmail.com>
2026-01-29 15:24:40 +0200

    print_2d

---
+++ Nesting/B_Nesting/print_2d.py
@@ -0,0 +1,32 @@
+def print2d(matrix):
+    for row in matrix:
+        for item in row:
+            print(item)
+
+array1 = [
+    ["a", "b", "c", "d"],
+    ["e", "f"],
+    ["g", "h", "i"]
+]
+
+print2d(array1)
+# prints:
+# a
+# b
+# c
+# d
+# e
+# f
+# g
+# h
+# i
+
+array2 = [[9, 3, 4], [11], [42, 100]]
+print2d(array2)
+# prints:
+# 9
+# 3
+# 4
+# 11
+# 42
+# 100
