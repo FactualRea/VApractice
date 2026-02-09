e5de6f4217dfbfe8e8deb5cb9d590729068ea646
Reatlegile <rmatsepe30@gmail.com>
2026-02-09 16:11:43 +0200

    email_parse

---
+++ Dictionary/A_Dictionary/email_parse.py
@@ -0,0 +1,15 @@
+def email_parse(email):
+    splitemail = {}
+    username, domain = email.split("@")
+    splitemail["username"] = username
+    splitemail["domain"] = domain
+    return splitemail
+
+print(email_parse("coolcoder42@goodmail.com"))
+# { 'username': 'coolcoder42', 'domain': 'goodmail.com' }
+
+print(email_parse("az@woohoomail.com"))
+# { 'username': 'az', 'domain': 'woohoomail.com' }
+
+print(email_parse("1337pr0graMmer@coldmail.edu"))
+# { 'username': '1337pr0graMmer', 'domain': 'coldmail.edu' }
