dbf62a7eaa50aaef5fddadd6406c3cc687a436f7
Reatlegile <rmatsepe30@gmail.com>
2026-02-11 14:02:50 +0200

    is prime

--- Decomposition/A_Decomposition/is_prime.py
+++ Decomposition/A_Decomposition/is_prime.py
@@ -1,4 +1,4 @@
-def is_prie(num):
+def is_prime(num):
     if num < 2:
         return False
     for i in range(2, int(num**0.5) + 1):
@@ -6,4 +6,10 @@ def is_prie(num):
             return False
     return True
 
-
+print(is_prime(11))# True
+print(is_prime(8))# False
+print(is_prime(7))# True
+print(is_prime(21))# False
+print(is_prime(2))# True
+print(is_prime(15))# False
+print(is_prime(1))# False
